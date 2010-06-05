6b42366102b6cda76d42b78550f9640bf1a2b387
Mariano Reingart <reingart@gmail.com>
2010-06-05 04:55:23 -0300

    Event major changes:  * Replaced t2.attachment, t2.comment, t2.review with plain tables and crud  * Added author table  * Decorated controllers with permissions, membership and custom security checks  * Fixed crud redirects with crud.settings.controller  * Removed t2.display  * Rewrote display view  * Added views attach and comment  * Simplified review view  * Added missing translations  * Minor fixes
    --HG--
    branch : jrsl

--- controllers/event.py
+++ controllers/event.py
@@ -2,60 +2,100 @@
 # Events (event proposal)
 #############################################
 
+# we are not in default controller, change it at crud
+crud.settings.controller='event'
 
 @auth.requires_login()
 def proposed():
     events=db(db.event.id>0).select(orderby=db.event.title)
-    return dict(events=events)
+    return dict(talks=events)
 
 def accepted():
     db.event['represent']=lambda event: A('%s by %s' % (event.title,event.authors),
        _href=URL(r=request,f='event_info',args=[event.id]))
     query=(db.event.status=='accepted')&(db.auth_user.id==db.event.created_by)
     rows=db(query).select(orderby=db.event.scheduled_datetime)
-    attachments=db(db.t2_attachment.table_name=='event').select()     
+    attachments=db(db.attachment.id>0).select()     
     attachs = {}
     for attach in attachments:
-        attachs.setdefault(attach.record_id, []).append(attach) 
+        attachs.setdefault(attach.event_id, []).append(attach) 
     return dict(rows=rows,attachs=attachs)
 
 @auth.requires_login()
 def propose():
-    return dict(form=crud.create(db.event, next='display_event/[id]'))
+    insert_author = lambda form: db.author.insert(user_id=auth.user_id,event_id=form.vars.id)
+    return dict(form=crud.create(db.event, 
+                                 next='display/[id]', 
+                                 onaccept=insert_author))
 
-@auth.requires_login()
+@auth.requires(auth.has_membership(role='manager') or user_is_author())
 def update():
     if not db(db.event.created_by==auth.user.id and db.event.id==request.args[0]).count():
         redirect(URL(r=reuqest,f='index'))
     form=crud.update(db.event, request.args[0],
-                     next='display_event/[id]',
+                     next='display/[id]',
                      ondelete=lambda form: redirect(URL(r=request,f='index')))
     return dict(form=form)
 
-@auth.requires_login()
-def display(): 
-    item=t2.display(db.event)
-    comments=t2.comments(db.event)
-    rows=db(db.event.id==request.args[0]).select()
-    if auth.has_membership(role='manager') or (rows and rows[0].created_by==auth.user.id):
-        writable=True
-    else:
-        writable=False
-    attachments=t2.attachments(db.event,writable=writable)
-    return dict(item=item,comments=comments,attachments=attachments)
+@auth.requires(auth.has_membership(role='manager') or user_is_author())
+def display():
+    event_id=request.args[0]
+    rows = db(db.event.id==event_id).select()
+    item=crud.read(db.event,event_id)
+    comments=db(db.comment.event_id==event_id).select()
+    attachments=db(db.attachment.event_id==event_id).select()
+    reviews=db(db.review.event_id==event_id).select()
+    return dict(event_id=event_id,item=item,reviews=reviews,attachments=attachments,comments=comments)
 
-def info(): 
-    item=t2.display(db.event,query=(db.event.id==auth.user.id)&(db.event.status=='accepted'))
+
+@auth.requires(auth.has_membership(role='reviewer') or event_is_accepted())
+def info():
+    event_id=request.args[0]
+    item=crud.read(db.event,event_id)
     return dict(item=item)
 
-@auth.requires_login()
-def review(): 
-    item=t2.display(db.event)
-    rows=db(db.event.id==request.args[0]).select()
-    if session.reviewer and rows and not rows[0].created_by==auth.user.id:
-        writable=True
+@auth.requires(auth.has_membership(role='reviewer') or user_is_author())
+def comment(): 
+    event = db(db.event.id==request.args[0]).select()[0]
+    db.comment.event_id.default=event.id
+    form=crud.create(db.comment, 
+                     next=URL(r=request,f='display',args=event.id))
+    return dict(event=event,form=form)
+
+@auth.requires(auth.has_membership(role='reviewer') or user_is_author())
+def attach(): 
+    event = db(db.event.id==request.args[0]).select()[0]
+    db.attachment.event_id.default=event.id
+    if len(request.args)>1:
+        attachs = db((db.attachment.event_id==event.id)&(db.attachment.id==request.args[1])).select()
     else:
-        writable=True
-    attachments=t2.attachments(db.event,writable=True)
-    reviews=t2.reviews(db.event,writable=writable)
-    return dict(item=item,reviews=reviews,attachments=attachments)
+        attachs = None
+    if attachs:
+        form=crud.update(db.attachment, attachs[0].id,
+                         next=URL(r=request,f='display',args=event.id),
+                         ondelete=lambda form: redirect(URL(r=request,c='default',f='index')))
+    else:
+        form=crud.create(db.attachment, 
+                         next=URL(r=request,f='display',args=event.id))
+    return dict(event=event,form=form)
+
+@auth.requires(auth.has_membership(role='reviewer') or not user_is_author())
+def review(): 
+    event = db(db.event.id==request.args[0]).select()[0]
+    reviews = db((db.review.event_id==event.id)&(db.review.created_by==auth.user_id)).select()
+    if reviews:
+        form=crud.update(db.review, reviews[0].id,
+                         next=URL(r=request,f='display',args=event.id),
+                         ondelete=lambda form: redirect(URL(r=request,c='default',f='index')))
+    else:
+        db.review.event_id.default=event.id
+        form=crud.create(db.review, 
+                         next=URL(r=request,f='display',args=event.id))
+    return dict(event=event,form=form)
+
+def download(): 
+    query = (db.attachment.file==request.args[0])&(db.event.id==db.attachment.id)
+    event = db(query).select(db.event.id,db.event.status)[0]
+    if event.status=='accepted' or auth.has_membership(role='reviewer') or user_is_author(event.id):
+        return response.download(request,db)
+    raise HTTP(501)
--- models/db_event.py
+++ models/db_event.py
@@ -27,6 +27,7 @@ db.define_table('event',
              default=('%s %s' % (auth.user.first_name,auth.user.last_name)) if auth.user else ''),
     db.Field('modified_by','integer',label=T("Modified By"),readable=False,writable=False,default=auth.user.id if auth.user else 0),
     db.Field('modified_on','datetime',label=T("Modified On"),readable=False,writable=False,default=request.now,update=request.now),
+    format='%(title)s',
     migrate=migrate)
 
 db.event.description.display=lambda value: XML(value)
@@ -41,8 +42,71 @@ db.event.abstract.requires=IS_NOT_EMPTY()
 db.event.description.requires=IS_NOT_EMPTY()
 db.event.categories.widget=lambda s,v:T2.tag_widget(s,v,EVENT_CATEGORIES)
 ##db.event.displays=db.proposal.fields
+db.event.status.writable=db.event.status.readable=auth.has_membership('manager')
+db.event.scheduled_datetime.writable=db.event.scheduled_datetime.readable=auth.has_membership('manager')
+db.event.video.writable=db.event.video.readable=auth.has_membership('reviewer')
+
 db.event.represent=lambda event: \
    A('[%s] %s' % (event.status,event.title),
      _href=URL(r=request,c='event',f='display',args=[event.id]))
 
 db.define_table('event_archived',db.event,db.Field('event_proposal',db.event), migrate=migrate)
+
+db.define_table('attachment',
+   db.Field('event_id',db.event,label=T('Event'),writable=False),
+   db.Field('name','string',label=T('Name')),
+   db.Field('description','text',label=T('Description')),
+   db.Field('file','upload',label=T('File')),
+   db.Field('file_data','blob',default=''),
+   db.Field('filename'),
+   db.Field('created_by','integer',label=T("Created By"),readable=False,writable=False,default=auth.user.id if auth.user else 0),
+   db.Field('created_on','datetime',label=T("Created On"),readable=False,writable=False,default=request.now),
+   migrate=migrate)
+db.attachment.name.requires=IS_NOT_EMPTY()
+db.attachment.file.requires=IS_NOT_EMPTY()
+db.attachment.filename.requires=IS_NOT_EMPTY()
+db.attachment.filename.comment=T("(new filename for downloads)")
+
+db.define_table('comment',
+   db.Field('event_id',db.event,label=T('Event'),writable=False),
+   db.Field('body','text',label=T('Body')),
+   db.Field('created_signature',label=T("Created Signature"),readable=False,writable=False,
+             default=('%s %s' % (auth.user.first_name,auth.user.last_name)) if auth.user else ''),
+   db.Field('created_by','integer',label=T("Created By"),readable=False,writable=False,default=auth.user.id if auth.user else 0),
+   db.Field('created_on','datetime',label=T("Created On"),readable=False,writable=False,default=request.now),
+   migrate=migrate)
+db.comment.body.requires=IS_NOT_EMPTY()
+
+db.define_table('review',
+   db.Field('event_id',db.event,label=T('Event'),writable=False),
+   db.Field('rating','integer',label=T('Rating'),default=0),
+   db.Field('body','text',label=T('Body')),
+   db.Field('created_by','integer',label=T("Created By"),readable=False,writable=False,default=auth.user.id if auth.user else 0),
+   db.Field('created_signature',label=T("Created Signature"),readable=False,writable=False,
+             default=('%s %s' % (auth.user.first_name,auth.user.last_name)) if auth.user else ''),
+   db.Field('created_on','datetime',label=T("Created On"),readable=False,writable=False,default=request.now),
+   migrate=migrate)
+db.review.body.requires=IS_NOT_EMPTY()
+db.review.rating.requires=IS_IN_SET([str(x) for x in range(0,6)])
+## db.review.rating.widget=T2.rating_widget
+
+db.define_table('author',
+    db.Field('user_id', db.auth_user),
+    db.Field('event_id', db.event),
+    db.Field('created_by','integer',label=T("Created By"),readable=False,writable=False,default=auth.user.id if auth.user else 0),
+    db.Field('created_on','datetime',label=T("Created On"),readable=False,writable=False,default=request.now),
+    migrate=migrate)
+    
+def user_is_author(event_id=None):
+    if not auth.is_logged_in() or (not request.args and event_id is None):
+        return False
+    if event_id is None:
+        event_id = request.args[0]
+    if db((db.author.user_id==auth.user_id)&(db.author.event_id==event_id)).count():
+        return True
+
+def event_is_accepted():
+    if not request.args:
+        return False
+    if db((db.event.id==request.args[0])&(db.event.status=='accepted')).count():
+        return True
