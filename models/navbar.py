844eac52798a9bd4ac1f931e7db3e9a8da51d63d
Mariano Reingart <reingart@gmail.com>
2012-04-07 16:39:04 -0300

    added navbar enable/disable const

--- models/navbar.py
+++ models/navbar.py
@@ -1,36 +1,39 @@
 # coding: utf8
 
-db.define_table('navbar',
-    Field("title", "string"),
-    Field("url", "string", requires=IS_EMPTY_OR(IS_URL())),
-    Field("c", label="Controller"), 
-    Field("f", label="Function"), 
-    Field("args", label="Arguments"), 
-    Field("sortable", "integer"),
-    Field("parent_id", "reference navbar"),
-    format="%(title)s",
-    migrate=migrate,
-    )
+# dynamic menu (enable in 0.py, remember to add navbar records!)
 
-def get_sub_menus(parent_id, default_c=None, default_f=None):
-    children = db(db.navbar.parent_id==parent_id)
-    for menu_entry in children.select(orderby=db.navbar.sortable):
-        # get action or use defaults:
-        c = menu_entry.c or default_c
-        f = menu_entry.f or default_f 
-        # is this entry selected? (current page)
-        sel = (request.controller==c and request.function==f and   
-          (request.args and request.args==menu_entry.args or True))
-        # return each menu item
-        yield (T(menu_entry.title), 
-         sel, menu_entry.url or URL(c, f, args=menu_entry.args), 
-         get_sub_menus(menu_entry.id, c, f)
-         )         
+if NAVBAR:
+    db.define_table('navbar',
+        Field("title", "string"),
+        Field("url", "string", requires=IS_EMPTY_OR(IS_URL())),
+        Field("c", label="Controller"), 
+        Field("f", label="Function"), 
+        Field("args", label="Arguments"), 
+        Field("sortable", "integer"),
+        Field("parent_id", "reference navbar"),
+        format="%(title)s",
+        migrate=migrate,
+        )
 
-# if navbar record, use them, if not, use default menu
-menu_info = list(get_sub_menus(parent_id=None))
-if menu_info:
-    response.menu = menu_info
+    def get_sub_menus(parent_id, default_c=None, default_f=None):
+        children = db(db.navbar.parent_id==parent_id)
+        for menu_entry in children.select(orderby=db.navbar.sortable):
+            # get action or use defaults:
+            c = menu_entry.c or default_c
+            f = menu_entry.f or default_f 
+            # is this entry selected? (current page)
+            sel = (request.controller==c and request.function==f and   
+              (request.args and request.args==menu_entry.args or True))
+            # return each menu item
+            yield (T(menu_entry.title), 
+             sel, menu_entry.url or URL(c, f, args=menu_entry.args), 
+             get_sub_menus(menu_entry.id, c, f)
+             )         
+
+    response.menu = list(get_sub_menus(parent_id=None))
+    
+    # add standard statistics and user submenus:
+    
     submenu_info=[
             [T('Companies'),False,URL(r=request,c='stats',f='companies')],
             [T('Attendees'),False,URL(r=request,c='stats',f='attendees')],
