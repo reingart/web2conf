7bb50071bfe64982269f2e94b0878178463a2de0
Mariano Reingart <reingart@gmail.com>
2012-08-28 04:07:22 -0300

    added more enhancements to schedule (no need to hardcode slots, row span, hide unused rooms, special cases -sprint/social days)
    --HG--
    branch : pyconar2012

--- controllers/schedule.py
+++ controllers/schedule.py
@@ -9,11 +9,11 @@ def index():
         text = text.decode('utf-8')
         if len(text) > maxlen:
             pre = max(0, (maxlen-3))
-            text = text[:pre] + '...' 
+            text = text[:pre] + '...'
         return text.encode('utf8')
-        
+
     q = db.activity.type!='poster'
-    q &= db.activity.type!='project'
+    #q &= db.activity.type!='project'
     q &= db.activity.status=='accepted'
     q &= db.activity.scheduled_datetime!=None
     rows = db(q).select(db.activity.id,
@@ -25,20 +25,41 @@ def index():
                         db.activity.type,
                         db.activity.created_by,
                         db.activity.authors,
-                        db.activity.categories ,
+                        db.activity.categories,
+                        db.activity.duration,
+                        db.activity.confirmed,
                         db.activity.scheduled_datetime,
                         db.activity.scheduled_room,
                         )
     levels = {}
     for i, level in enumerate(ACTIVITY_LEVELS):
         levels[level] = XML("&loz;"* (i+1),)
-        
+
     activities_per_date = {}
-    for row in rows:
-        activities_per_date.setdefault(\
-            row.scheduled_datetime.date(), \
-            []).append(row)
-            
+    slots_per_date = {}
+    rooms_per_date = {}
+
+    for activity in rows:
+        date = activity.scheduled_datetime.date()
+        time = activity.scheduled_datetime.time()
+        room = int(activity.scheduled_room)
+        activities_per_date.setdefault(date, []).append(activity)
+        if not room in rooms_per_date.get(date, []):
+            rooms_per_date.setdefault(date, []).append(room)
+        if time not in slots_per_date.get(date, []):
+            slots_per_date.setdefault(date, {}).setdefault(time, {})
+            # find overlapped slots
+            if activity.duration and activity.type not in ('open space', 'project', 'sprint', 'social'):
+                for i in range(activity.duration/60):
+                    hidden_slot = activity.scheduled_datetime + datetime.timedelta(minutes=60*i)
+                    hidden_slot_time = hidden_slot.time()
+                    if hidden_slot_time not in slots_per_date.get(date, []):
+                        slots_per_date[date].setdefault(hidden_slot_time, {})
+            elif activity.duration:
+                # record duration for special activities (social, meetings, sprints, etc.)
+                slots_per_date[date][time] = activity.duration
+
+    
     rooms = ACTIVITY_ROOMS.copy()
 
     schedule_tables = {}
@@ -47,22 +68,35 @@ def index():
     schedule = dict([((row.scheduled_datetime,
                        row.scheduled_room),
                       row.id) for row in rows])
-    
+
     ##fields.append(BEAUTIFY(schedule))
     hidden = []
 
-    for day in DAYS:
+    for day in sorted(activities_per_date.keys()):
         table = []
-        th = [TH("")] + [TH(room) for room in rooms.values()]
+        th = [TH("")] + [TH(name) for room, name in rooms.items()
+                         if room in rooms_per_date[day]]
         table.append(THEAD(TR(*th)))
-        for slot in SLOTS:
-            tr = [TD(slot)]
+        slots = sorted(slots_per_date[day])
+        for slot in slots:
+            if len(slots)==1 and slots_per_date.get(day, {}).get(slot):
+                slot_duration = slots_per_date[day][slot]
+                slot_end = datetime.datetime.combine(day, slot) + datetime.timedelta(minutes=slot_duration)
+                caption = T("%s to %s") % (slot.strftime("%H:%M"), 
+                                        slot_end.strftime("%H:%M"))
+            else:
+                caption = slot.strftime("%H:%M")            
+            tr = [TD(caption, _width="5%", _style="text-align: center;", )]
             common = None
+            width = "%d%%" % (100/len(rooms_per_date[day])-5)
             for room in rooms:
-                dt = datetime.datetime.strptime("%s %s" % (day,
-                                                           slot),
-                                                "%Y-%m-%d %H:%M")
-                selected = schedule.get((dt, str(room)))
+                if not room in rooms_per_date[day]:
+                    continue   # hide unused rooms
+                # find an activity for this slot
+                slot_dt = datetime.datetime.combine(day, slot)
+                selected = schedule.get((slot_dt, str(room)))
+                if selected == False:
+                    continue   # spanned row!
                 activity = selected and activities[int(selected)] \
                            or None
 
@@ -93,14 +127,14 @@ def index():
                         authors = u.link(cram(activity.authors, 25))
                     else:
                         authors = ''
-                        
+
                     a = PluginMModal(title=activity.title, \
                             content=MARKMIN(activity.abstract or \
                                             '').xml(),
                                      close=T('close'),
                                      width=50, height=50)
                     hidden.append(a)
-                    
+
                     td = TD(
                         a.link(B(cram(activity.title, 50))),
                                BR(),
@@ -112,142 +146,43 @@ def index():
                                (', '.join(activity.categories or \
                                 [])),
                                BR(), "", authors, "") or "",
+                               _width=width,
                                _style="text-align: center;",
+                               _id=not activity.confirmed and "unconfirmed" or "confirmed",
                                _class="%s %s" % (activity.track,
-                                                 activity.type.replace(" ", "_")))
+                                                 activity.type.replace(" ", "_"),
+                                                 ))
                     if activity.type in ACTIVITY_COMMON:
                         tr = [tr[0],]
                         td.attributes["_colspan"] = len(rooms)
                         tr.append(td)
                         break
                     else:
+                        if activity.duration:
+                            slot_span = 0
+                            for next_slot in slots[slots.index(slot)+1:]:
+                                next_slot_dt = datetime.datetime.combine(slot_dt.date(), next_slot)
+                                if next_slot_dt > (slot_dt  + datetime.timedelta(minutes=activity.duration-1)):
+                                    break
+                                else:
+                                    # mark the slot as spanned
+                                    schedule[(next_slot_dt, str(room))] = False
+                                    slot_span += 1
+                            if slot_span:
+                                td.attributes["_rowspan"] = slot_span + 1
                         tr.append(td)
                 else:
-                    tr.append(TD())                    
+                    tr.append(TD(_width=width))
 
             table.append(TR(*tr))
-            
-        day = datetime.datetime.strptime(day, "%Y-%m-%d")
+
         schedule_tables[day] = TABLE(*table, _class="schedule")
-                
+
     d = dict(activities_per_date=activities_per_date,
              levels=levels, hidden=hidden,
              schedule_tables=schedule_tables)
     return response.render(d)
-    
-#@cache(request.env.path_info,time_expire=60,cache_model=cache.ram)
-#@auth.requires_membership(role="manager")
-def index_alan():
-    query = (db.activity.status=='accepted') & \
-            (db.auth_user.id==db.activity.created_by)
-    query &= db.activity.scheduled_datetime != None
-    query &= db.activity.scheduled_room != None
-    
-    rows = db(query).select(orderby=(db.activity.scheduled_datetime,
-                          db.activity.scheduled_room))
 
-    activities_per_date = {}
-    for row in rows:
-        activities_per_date.setdefault(\
-            row.activity.scheduled_datetime.date(), \
-            []).append(row)
-
-    db.activity['represent']=lambda activity: \
-        A('%s by %s' % (activity.title,activity.authors),
-          _href=URL(r=request,f='activity_info',args=[activity.id]))
-
-    levels = {}
-    for i, level in enumerate(ACTIVITY_LEVELS):
-        levels[level] = XML("&loz;"* (i+1),)
-
-    # TODO move this code to the view
-    schedule_tables = {}
-    for day in SCHEDULE_FRAME.keys():
-        day_as_date = datetime.date(*[int(s) for s in day.split("-")])
-        slots = schedule_slots(day)
-        headers = [TH(),]
-        position_track = {0: None}
-        position_spans = {0: None}
-        for i, room in enumerate(sorted(ACTIVITY_ROOMS.keys())):
-            headers.append(TH("%s" % (ACTIVITY_ROOMS[room],)))
-            SCHEDULE_FRAME[day]["rooms"][room] = ro
-            position_track[i+1] = SCHEDULE_FRAME[day]["rooms"][room]
-            position_spans[i+1] = 0
-            
-        table_width = len(headers)
-        thead = THEAD(TR(*headers))
-        trs = []
-
-        if day_as_date in activities_per_date.keys():
-            for i_dt, dt in enumerate(slots):
-                tds = []
-                activity_start = False
-                start_of_activity = False
-                common = False
-                # Which activities start here?
-                for i, track in position_track.iteritems():
-                    if common:
-                        cell = True
-                    else:
-                        cell = False
-                    if (i != 0)  and (common == False):
-                        for row in activities_per_date[day_as_date]:
-                            if cell == False:
-                                this_track = row.activity.track == track
-                                is_next = row.activity.scheduled_datetime >= dt
-                                is_common = row.activity.type in ACTIVITY_COMMON
-                                if (this_track or is_common) and is_next:
-                                    try:
-                                        next_slot = slots[i_dt + 1]
-                                        activity_start = row.activity.scheduled_datetime < next_slot
-                                    except IndexError, e:
-                                        # end of time slots!
-                                        activity_start = True
-                                    if activity_start:
-                                        if is_common:
-                                            common = is_common
-                                            colspan = table_width -1
-                                        else:
-                                            colspan = None
-                                        rowspan = schedule_activity_spans(row.activity.duration)
-                                        if rowspan > 0:
-                                            position_spans[i] = rowspan
-                                        tds.append(TD(row.activity.title,
-                                                        _rowspan=rowspan,
-                                                        _colspan=colspan,
-                                                        _class=row.activity.type.replace(" ", "-")))
-                                        cell = True
-                                        start_of_activity = True
-
-                        if not cell:
-                            if not position_spans[i] > 0:
-                                tds.append(TD(_class="empty"))
-                            else:
-                                position_spans[i] -= 1
-
-                if start_of_activity:
-                    tr_class = "odd"
-                    # mark the time at the first cell
-                    tds.insert(0, TD(dt, _class="time"))
-                else:
-                    tr_class = None
-                    tds.insert(0, TD(_class="empty"))
-
-                # *list function argument is not working
-                # trs.append(TR(*tds, _class=tr_class))
-                a_tr = TR(_class=tr_class)
-                [a_tr.append(td) for td in tds]
-                trs.append(a_tr)
-
-        tbody = TBODY(*trs)
-
-        schedule_tables[day] = TABLE(thead, tbody, _class="schedule")
-
-    d = dict(activities_per_date=activities_per_date,
-             levels=levels,
-             schedule_tables=schedule_tables)
-             
-    return response.render(d)
 
 @auth.requires_membership(role="manager")
 def agenda():
@@ -269,7 +204,7 @@ def agenda():
                                         db.activity.scheduled_datetime,
                                         db.activity.scheduled_room,
                                         orderby=order)
-    
+
     rooms = ACTIVITY_ROOMS.copy()
     statuses = ['pending','accepted','rejected', 'declined']
     rooms[None] = ""
@@ -292,9 +227,9 @@ def agenda():
                            opt in sorted(rooms.keys())],
                            _name='room.%s' % row.id), BR()])
     fields.append(INPUT(_type="submit"))
-    
+
     form = FORM(*fields)
-        
+
     out = []
     if form.accepts(request.vars, session):
         response.flash = 'form accepted'
@@ -314,7 +249,7 @@ def agenda():
     elif form.errors:
         response.flash = 'form has errors'
 
-    
+
     return dict(form=form, out=out)
 
 
@@ -326,17 +261,17 @@ def agenda2():
                                         db.activity.scheduled_datetime,
                                         db.activity.scheduled_room,
                                         orderby=db.activity.title)
-    
+
     activities = sorted([(row.id, row.title) for row in rows], key=lambda x: x[1])
     rooms = sorted([(0, "")] + list(ACTIVITY_ROOMS.items()), key=lambda x: x[1])
-    
+
 
     form = SQLFORM.factory(
         Field("activity_id", 'integer', requires=IS_IN_SET(activities)),
         Field("room", 'integer', requires=IS_IN_SET(rooms)),
         Field("datetime", 'datetime'),
         )
-        
+
     if form.accepts(request.vars, session, keepvalues=True):
         response.flash = 'form accepted'
         db(db.activity.id==form.vars.activity_id).update(
@@ -369,7 +304,7 @@ def grid():
     activities = {None: ""}
     activities.update(dict([(row.id, row.title) for row in rows]))
     schedule = dict([((row.scheduled_datetime, row.scheduled_room), row.id) for row in rows])
-    
+
     ##fields.append(BEAUTIFY(schedule))
 
     table = []
@@ -382,25 +317,25 @@ def grid():
                 dt = datetime.datetime.strptime("%s %s" % (day, slot), "%Y-%m-%d %H:%M")
                 selected = schedule.get((dt, str(room)))
                 selected = selected and int(selected)
-                
+
                 tr.append(
-                    TD( 
+                    TD(
                         SELECT([OPTION(v,
                                    _value=k,
                                    _selected=k and int(k)==selected) for \
-                                   (k, v) in sorted(activities.items(), 
+                                   (k, v) in sorted(activities.items(),
                                                     key=lambda x: x[1].lower())],
                                    _name='slot.%s.%s.%s' % (day, slot, room),
                                    _style='width: 100px')
                     ))
             table.append(TR(*tr))
-    
+
     fields.append(TABLE(*table))
-    
+
     fields.append(INPUT(_type="submit"))
-    
+
     form = FORM(*fields)
-        
+
     out = []
     if form.accepts(request.vars, session, keepvalues=True):
         response.flash = 'form accepted'
@@ -415,10 +350,10 @@ def grid():
                 q = db.activity.scheduled_datetime == dt
                 q &= db.activity.scheduled_room == slot_room
                 db(q).update(scheduled_datetime=None, scheduled_room=None)
-                # allocate activity (if selected for this slot) 
+                # allocate activity (if selected for this slot)
                 if activity_id:
                     q = db.activity.id==activity_id
-                    db(q).update(scheduled_datetime=dt, 
+                    db(q).update(scheduled_datetime=dt,
                                  scheduled_room=slot_room)
                 out.append("setting %s = d%s %s %s" % (activity_id, slot_day, slot_time, slot_room))
             pass
@@ -426,5 +361,5 @@ def grid():
     elif form.errors:
         response.flash = 'form has errors'
 
-    
+
     return dict(form=form, out=out)
